4b117287dc06ce6d6aa78a8bb585b7971029e806
omochimetaru <omochi.metaru@gmail.com>
2022-12-16 23:07:02 +0900

    implement for linux

--- Sources/codegen/Codegen.swift
+++ Sources/codegen/Codegen.swift
@@ -42,10 +42,11 @@ struct Codegen {
         let sourcesDir = URL(fileURLWithPath: sourcesDirString)
 
         let fm = FileManager.default
-        try enumrateDirectory(sourcesDir, fileManager: fm) { (file) in
+
+        for file in fm.enumerateRelative(path: sourcesDir, options: [.skipsHiddenFiles]) {
             var isDir = ObjCBool(false)
             guard fm.fileExists(atPath: file.path, isDirectory: &isDir),
-                  !isDir.boolValue else { return }
+                  !isDir.boolValue else { continue }
 
             if file.lastPathComponent.hasSuffix(".swift") {
                 try render(file: file)
@@ -63,28 +64,6 @@ struct Codegen {
                 )
                 try rendererType.init(writer: writer).render()
             }
-        }
-    }
-
-    private func enumrateDirectory(
-        _ directory: URL,
-        fileManager fm: FileManager = .default,
-        body: (URL) throws -> Void
-    ) throws {
-        guard fm.changeCurrentDirectoryPath(directory.path) else {
-            throw MessageError("failed to chdir: \(directory.path)")
-        }
-
-        guard let en = fm.enumerator(
-            at: URL(fileURLWithPath: "."),
-            includingPropertiesForKeys: nil,
-            options: [.skipsHiddenFiles, .producesRelativePathURLs]
-        ) else {
-            throw MessageError("failed to enumerate")
-        }
-
-        for case let file as URL in en {
-            try body(file)
         }
     }
 }
