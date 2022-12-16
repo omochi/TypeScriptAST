40998a2b53f48cb32799287f069b45b26e934218
omochimetaru <omochi.metaru@gmail.com>
2022-12-16 22:04:29 +0900

    refine

--- Sources/codegen/Codegen.swift
+++ Sources/codegen/Codegen.swift
@@ -65,4 +65,26 @@ struct Codegen {
             }
         }
     }
+
+    private func enumrateDirectory(
+        _ directory: URL,
+        fileManager fm: FileManager = .default,
+        body: (URL) throws -> Void
+    ) throws {
+        guard fm.changeCurrentDirectoryPath(directory.path) else {
+            throw MessageError("failed to chdir: \(directory.path)")
+        }
+
+        guard let en = fm.enumerator(
+            at: URL(fileURLWithPath: "."),
+            includingPropertiesForKeys: nil,
+            options: [.skipsHiddenFiles, .producesRelativePathURLs]
+        ) else {
+            throw MessageError("failed to enumerate")
+        }
+
+        for case let file as URL in en {
+            try body(file)
+        }
+    }
 }
