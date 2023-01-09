8f5e8c8029af97405c337d8e1d97aea12d479987
omochimetaru <omochi.metaru@gmail.com>
2023-01-10 07:52:20 +0900

    support ts extension

--- Sources/TypeScriptAST/Dependency/ImportFileExtension.swift
+++ Sources/TypeScriptAST/Dependency/ImportFileExtension.swift
@@ -1,4 +1,13 @@
-public enum ImportFileExtension {
+public enum ImportFileExtension: CustomStringConvertible {
     case none
     case js
+    case ts
+
+    public var description: String {
+        switch self {
+        case .none: return ""
+        case .js: return "js"
+        case .ts: return "ts"
+        }
+    }
 }
